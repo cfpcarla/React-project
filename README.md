8aadff0f45ee57ba723d8dc565b89e11bcc8acd0
carla <cfpcarla@gmail.com>
2020-07-22 12:44:38 -0400

    Implement logout page and validation for email

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # React-project
+
+#https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+
+- The page is responsive React application with Material Ui
+
+![login_page](/photos/login.png)
+
+![logout_page](/photos/logout.png)
+
+## Technologies
+
+Project is created with:
+
+- React with materialUI
+
+```
+1. Reset database: `npm run db:reset`
+  - Check the db folder to see what gets created and seeded in the SDB
+2. Run the server: `npm run local`
+  - Note: nodemon is used, so you should not have to restart your server
+3. Visit `http://localhost:3000/`
+```
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, TextField, Box, Grid } from "@material-ui/core";
+import { Button, TextField, Box, Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -9,87 +9,108 @@ const useStyles = makeStyles((theme) => ({
       width: "25ch",
     },
   },
+  text: {
+    padding: "20px",
+  },
 }));
 
-// function handleLogout() {
-//   props.setEmail(null);
-//props.setPassword(null);
-//   localStorage.removeItem('email');
-//   window.location = '/';
-// }
-
-//fazer grid
-//mostrar email da pessoa
-//esconder botao
-
-{
-  /* <div key={0}>
-    <Button
-    color="danger"
-    block
-    onClick={() => handleLogout()}
-    >Logout</Button>
-    </div>,
-    <div key={1} ></div> */
-}
+const LogoutButton = ({ logoutFn }) => {
+  return (
+    <Box p="60px">
+      <Button
+        variant="button"
+        size="large"
+        width="60%"
+        backgroundColor="#fcff4a"
+        fullWidth
+        block
+        gutterBottom
+        onClick={logoutFn}
+      >
+        Logout
+      </Button>
+    </Box>
+  );
+};
 
 export default function App(props) {
   const classes = useStyles();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [display, setDisplay] = useState("login");
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return email.length > 0 && password.length > 0 && validateEmail(email);
+  }
+  function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
   }
 
   function handleSubmit(e) {
     e.preventDefault();
+    setDisplay("logout");
     localStorage.setItem("email", JSON.stringify(email));
   }
 
   return (
     <Grid container spacing={1}>
-      <form
-        onSubmit={handleSubmit}
-        className={classes.root}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          required
-          id="email"
-          label="Email"
-          defaultValue="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+      <Box display={display === "login" ? "block" : "none"}>
+        <form
+          onSubmit={handleSubmit}
+          className={classes.root}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            required
+            id="email"
+            label="Email"
+            defaultValue="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            id="pass"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+          <Box p="60px">
+            <Button
+              variant="button"
+              size="large"
+              width="60%"
+              backgroundColor="#fcff4a"
+              fullWidth
+              block
+              gutterBottom
+              className={classes.root}
+              disabled={!validateForm(props)}
+              type="submit"
+            >
+              Login
+            </Button>
+          </Box>
+        </form>
+      </Box>
+      <Box display={display === "logout" ? "block" : "none"}>
+        <Typography className={classes.text} variant="body5">
+          You have successfully logged in as {email}
+        </Typography>
+        <LogoutButton
+          logoutFn={() => {
+            localStorage.removeItem("email");
+            setEmail("");
+            setPassword("");
+            setDisplay("login");
+          }}
         />
-        <TextField
-          id="pass"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-        />
-        <Box p="60px">
-          <Button
-            variant="button"
-            size="large"
-            width="60%"
-            backgroundColor="#fcff4a"
-            fullWidth
-            block
-            gutterBottom
-            className={classes.root}
-            disabled={!validateForm(props)}
-            type="submit"
-          >
-            Login
-          </Button>
-        </Box>
-      </form>
+      </Box>
     </Grid>
   );
 }
